95a1c993a56ba47bd906d23df51af751395204c3
brandon <brancarp93@gmail.com>
2024-10-19 18:30:53 -0400

    form update

---
+++ app/login/page.tsx
@@ -0,0 +1,11 @@
+// Form using react-hook-forms + zod
+
+
+export default function login() {
+  return <>
+    <div>
+      <h1>Login Here</h1>
+
+    </div>
+  </>
+}
---
+++ components/ui/navbar.tsx
@@ -0,0 +1,7 @@
+export default function navbar() {
+  return (
+    <>
+      <h1>Nav bar</h1>
+    </>
+  )
+}
