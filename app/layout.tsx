ac245184652e0582546ceffc69ac06420ce6dcdc
brandon <brancarp93@gmail.com>
2024-10-27 20:10:53 -0400

    updated font to roboto

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,5 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -20,13 +8,14 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white text:black  dark:bg-gray-950 dark:text-white`}
+        className={`antialiased bg-white text-black dark:bg-gray-950 dark:text-white`}
+        style={{ fontFamily: "'Raleway', 'Roboto', sans-serif" }}
       >
         {children}
       </body>
