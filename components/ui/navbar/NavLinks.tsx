e5708800d5779f14f2fcf4c763825f3e6d111b5b
brandon <brancarp93@gmail.com>
2025-01-16 21:09:51 -0500

    nav update

--- components/ui/navbar/NavLinks.tsx
+++ components/ui/navbar/NavLinks.tsx
@@ -7,14 +7,14 @@ export default function NavLinks() {
     <>
       <div className="flex justify-between items-center ">
         <div className="flex items-center">
-          <ul className="flex text-[13px] space-x-2">
+          <ul className="flex text-[13px] space-x-2 md:text-[1rem]">
             {/* <li className="relative group cursor-pointer">
               Calendar
               <span className="absolute bottom-0 left-0 w-0 h-[2px] bg-indigo-600 transition-all duration-300 group-hover:w-full"></span>
             </li> */}
             <li className="relative group cursor-pointer">
               Blogs
-              <span className="absolute bottom-0 left-0 w-0 h-[2px] bg-indigo-600 transition-all duration-300 group-hover:w-full"></span>
+              <span className="absolute bottom-0 left-0 w-0 h-[2px] bg-indigo-600 transition-all duration-300 group-hover:w-full "></span>
             </li>
             <li className="relative group cursor-pointer">
               Sign in
--- components/ui/navbar/Navbar.tsx
+++ components/ui/navbar/Navbar.tsx
@@ -6,7 +6,7 @@ export default function Navbar() {
   return (
     <>
       <div className=" flex justify-between items-center p-3">
-        <h1 className="semi-bold">Zen Mindset</h1>
+        <h1 className="semi-bold md:text-[1rem]">Zen Mindset</h1>
         <div className="flex items-center ">
           <NavLinks />
 
